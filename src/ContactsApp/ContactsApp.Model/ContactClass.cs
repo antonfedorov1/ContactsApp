c2ecb09bb5b347e00992f08875cc02ee03d2ac3c
Антон 0_0 <super.vanioks@yandex.ru>
2023-03-31 12:16:35 +0700

    add<ContactClass, ProjectClass, ProjextManagerClass> - добавлены три класса

---
+++ src/ContactsApp/ContactsApp.Model/ContactClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
+{
+    internal class ContactClass
+    {   
+        private string _fullName;
+        private string _eMail;
+        private string _phoneNumber;
+        private string _dateOfBirth;
+        private string _idVK;
+
+        
+    }
+}
