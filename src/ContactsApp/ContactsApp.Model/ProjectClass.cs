ea2660fdd0e6e2b5fc6955bbfc5dbac32ae9f409
Антон 0_0 <super.vanioks@yandex.ru>
2023-04-15 00:51:27 +0700

    feature(ProjectClass) - написал класс для работы со списком контактов

--- src/ContactsApp/ContactsApp.Model/ProjectClass.cs
+++ src/ContactsApp/ContactsApp.Model/ProjectClass.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ContactsApp.Model
+﻿namespace ContactsApp.Model
 {
-    internal class ProjectClass
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Project
     {
+        /// <summary>
+        /// Список всех контактов
+        /// </summary>
+        private List<Contact> _contactsList;
+
+        /// <summary>
+        /// Возвращает список контактов или добавляет нового пользователя в список контактов 
+        /// </summary>
+        public List<Contact> Contacts 
+        {
+            get { return _contactsList; }
+            set 
+            {
+                _contactsList = SortContacts(value);
+            }
+        }
+
+        /// <summary>
+        /// Сортировка контактов
+        /// </summary>
+        public List<Contact> SortContacts(List<Contact> contactsList)
+        {
+            contactsList.Sort((c1, c2) => c1.FullName.CompareTo(c2.FullName));
+            return contactsList;
+        }
+
+        /// <summary>
+        /// Поиск именинников
+        /// </summary>
+        public List<Contact> SearchForBirthdays()
+        {
+            List<Contact> birthdays;
+            DateTime dateTime = DateTime.Now;
+            birthdays = _contactsList.FindAll(contact => contact.DateOfBirth.Date == dateTime);
+            return birthdays;
+        }
+
+        /// <summary>
+        /// Поиск именинников
+        /// </summary>
+        public List<Contact> SearchContact(string fullName)
+        {
+            return _contactsList; //дописать
+        }
     }
 }
